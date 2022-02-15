7694d8d3f0a320857fd8797672d7170fd6c44bce
abu-bakarr <abangura14@yahoo.com>
2022-02-15 11:52:40 +0000

    Added file 0

--- 0-subs.py
+++ 0-subs.py
@@ -3,13 +3,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    ''' Finds the number of subscribers in a subreddit '''
-
-    header = {'User-Agent':  'Chrome/66.0.3359.139 Mobile Safari/537.36'}
-    url = "https://api.reddit.com/r/{}/about/".format(subreddit)
-    response = requests.get(url, headers=header)
-    if response.status_code == 200:
-        subs = response.json()["data"]["subscribers"]
-    else:
-        subs = 0
-    return subs
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            return info.get('data', {}).get('subscribers', 0)
+        except Exception:
+            return 0
